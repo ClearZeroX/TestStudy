7b96b07ff796d219ccc46604343f4c016ef90c54
ZhaLiCun <t_zhalc@channelsoft.com>
2018-10-25 16:40:45 +0800

    赋值顺序

--- src/main/java/com/zlc/po/Data.java
+++ src/main/java/com/zlc/po/Data.java
@@ -13,4 +13,44 @@ public class Data {
     private TotalItem totalItem;
     private ArrayList<Project> project;
 
+    public int getRefreshTime() {
+        return refreshTime;
+    }
+
+    public void setRefreshTime(int refreshTime) {
+        this.refreshTime = refreshTime;
+    }
+
+    public TotalItem getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(TotalItem totalItem) {
+        this.totalItem = totalItem;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "refreshTime=" + refreshTime +
+                ", totalItem=" + totalItem +
+                ", project=" + project +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        TotalItem totalItem = new TotalItem();
+        //赋值顺序
+        data.setTotalItem(totalItem);
+        totalItem.setPercent("==========");
+        System.out.println(data);//Data{refreshTime=0, totalItem=TotalItem{otalIvrCode='null', totalCount='null', percent='=========='}, project=null}
+
+    }
+
+
 }
+
+
--- src/main/java/com/zlc/po/TotalItem.java
+++ src/main/java/com/zlc/po/TotalItem.java
@@ -11,4 +11,36 @@ public class TotalItem {
     private String totalCount;
     private String percent;
 
+    public String getOtalIvrCode() {
+        return otalIvrCode;
+    }
+
+    public void setOtalIvrCode(String otalIvrCode) {
+        this.otalIvrCode = otalIvrCode;
+    }
+
+    public String getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(String totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
+    }
+
+    @Override
+    public String toString() {
+        return "TotalItem{" +
+                "otalIvrCode='" + otalIvrCode + '\'' +
+                ", totalCount='" + totalCount + '\'' +
+                ", percent='" + percent + '\'' +
+                '}';
+    }
 }
